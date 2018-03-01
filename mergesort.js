9e33c0809570bf50726492eb36ca585d65effbb5
Destin McMurry <destinmcmurry@gmail.com>
2018-03-01 16:08:37 -0500

    Need to make recursive mergeSort solution

--- mergesort.js
+++ mergesort.js
@@ -1,20 +1,55 @@
 function mergeSort(array){
-   // if(wholeArray.length>1)
+  if (array.length < 2) {
+    return array;
+  }
+
+  // if (array.length === 1) {
+  //   return array;
+  // } else {
+  //   return mergeSort(merge(split(array)));
+  // }
 
 }
-function merge(arrFirst, arrSecond){
 
+
+function merge(arr){
+  // return one sorted array
+  let arrFirst = arr[0];
+  let arrSecond = arr[1];
+  let sorted = [];
+  
+    while (arrFirst.length && arrSecond.length) {
+    if (Math.min(arrFirst[0], arrSecond[0]) === arrFirst[0]) {
+      sorted.push(arrFirst[0]);
+      arrFirst = arrFirst.slice(1);
+      if (!arrFirst.length) {
+        sorted = sorted.concat(arrSecond);
+        break;
+      }
+    } else {
+      sorted.push(arrSecond[0]);
+      arrSecond = arrSecond.slice(1);
+      if (!arrSecond.length) {
+        sorted = sorted.concat(arrFirst);
+        break;
+      }
+    }
+  }
+
+  return sorted;
 }
+
+
 
 function split(wholeArray) {
 
-    /* your code here to define the firstHalf and secondHalf arrays */
+  let midpoint=Math.floor(wholeArray.length/2);
 
- var midpoint=Math.floor(arr.length/2);
+  let firstHalf=wholeArray.slice(0,midpoint);
+  let secondHalf=wholeArray.slice(midpoint);
 
- var firstHalf=wholeArray.slice(0,midpoint);
- var secondHalf=wholeArray.slice(midpoint);
-    return [firstHalf, secondHalf];
-  }
+  return [firstHalf, secondHalf];
+
+}
 
 
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,44 +1,40 @@
-describe('Split Array function', function() {
-    it('is able to split an array into two halves', function() {
-      // your code here 
-    });
+describe('Split Array function', function () {
+  it('is able to split an array into two halves', function () {
+    expect(split([1,2,3,4])).toEqual([[1,2],[3,4]]);
+    expect(split([1,2,3,4,5])).toEqual([[1,2],[3,4,5]]);
+  });
+});
+
+describe('Merge function', function () {
+  it('is able to merge two sorted arrays into one sorted array', function () {
+    expect(merge([[1,3,5], [2,4]])).toEqual([1,2,3,4,5]);
+    expect(merge([[2,5,7,8], [1,3,4,6]])).toEqual([1,2,3,4,5,6,7,8]);
+  });
+});
+
+describe('Merge sort function', function () {
+
+  // beforeAll(function () {
+  //     spyOn(swap).and.callThrough();
+  //     spyOn(compare).and.callThrough();
+  // });
+
+  it('handles an empty array', function () {
+    expect(mergeSort([])).toEqual([]);
   });
 
-  describe('Merge function', function(){
-    it('is able to merge two sorted arrays into one sorted array', function(){
-      // test the merging algorithm
-    });
 
-    describe('Merge sort function', function(){
-        it('is able to sort an array', function(){
-          // test the merging algorithm
-        });
-      
-    
-    // beforeAll(function () {
-    //     spyOn(swap).and.callThrough();
-    //     spyOn(compare).and.callThrough();
-    // });
-
-    it('handles an empty array', function(){
-      expect( mergeSort([]) ).toEqual( [] );
-    });
-
- 
-    it('handles one element', function(){
-    
+  it('handles one element', function () {
     // expect(swap.calls.count()).toEqual(0);
     // expect(compare.calls.count()).toEqual(0);
     expect(mergeSort([1])).toEqual([1]);
-    
-    });
+  });
 
-    let arr = [6,1,9,8,2];
-
-    it('handles five elements', function(){
+  let arr = [6, 1, 9, 8, 2];
+  it('handles five elements', function () {
     // expect(swap.calls.count()).toEqual(5);
     // expect(compare.calls.count()).toEqual(12);
     expect(mergeSort(arr)).toEqual(arr.sort());
-    });
+  });
 
 });
